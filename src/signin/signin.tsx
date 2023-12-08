ba75a08f4f5a4fc18acd3fb389affa1c4d45c64d
Parth Nagdev <parthnagdev1@gmail.com>
2023-12-08 05:57:10 +0530

    failed button signup

--- src/signin/signin.tsx
+++ src/signin/signin.tsx
@@ -20,25 +20,29 @@ export const Signup = () => {
   };
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-        <br />
-        <button type="submit" className="button-style">Sign Up</button>
-      </form>
-      <button className="button-style2">Cancel</button>
+    <div style={{ textAlign: 'center' }}>
+      <h1 style={{ color: 'red', fontSize: '50px', marginTop: '1px' }}>Effort Tracker</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <form onSubmit={handleSubmit} >
+          <h2 style={{ fontSize: '24px' }}>Sign Up</h2>
+          <label>
+            Username:
+            <input type="text" value={username} onChange={handleUsernameChange} style={{ fontSize: '18px' }} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" value={password} onChange={handlePasswordChange} style={{ fontSize: '18px' }} />
+          </label>
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button type="submit" className="button-style" style={{ width: '150px', height: '50px', margin: '10px', border: '1px solid black', borderRadius: '5px' }}>Sign Up</button>
+            <button className="button-style2" onClick={() => { setUsername(''); setPassword(''); }} style={{ width: '150px', height: '50px', margin: '10px', border: '1px solid black', borderRadius: '5px' }}>Cancel</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default Signup;
-
