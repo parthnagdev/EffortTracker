610d00cf8036fd6dff7dc5212c15076516b95e02
Rachit Nagdev <rachitnagdev@gmail.com>
2024-02-18 23:10:37 +0530

    renaming package com.effortracker

--- backend/src/main/java/com/efforttracker/DemoApplication.java
+++ backend/src/main/java/com/efforttracker/DemoApplication.java
@@ -1,4 +1,5 @@
-package com.notes.notes;
+package com.efforttracker;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
